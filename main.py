3740d479cd52825dc2a45d0cd4a71fea3cfa57b8
FelixIzm <for_cv@mail.ru>
2020-01-11 16:08:43 +0300

    firstcomm

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+import pymongo, json
+
+client = MongoClient('35.193.230.58',
+    username='felix', password='12345678', 
+    authSource='obd',authMechanism='SCRAM-SHA-1')
+
+db = client['obd']
+collection_name = 'data'
+records = db[collection_name]
+count_doc_start = records.count_documents({})
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+'''
+@app.get("/items")
+def ret_count():
+    return {"count_doc": records.count_documents({})}
+'''
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
